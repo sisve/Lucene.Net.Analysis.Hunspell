9985513487b38224f144e8f48a9b224d8aabc3f7
Yann ROBIN <me.show@gmail.com>
2012-05-11 18:22:27 +0200

    Add support to parse alias flag Add test on parse alias flag (no exception test)

---
+++ tests/HunspellDictionaryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Lucene.Net.Analysis.Hunspell.Tests.Helpers;
+
+namespace Lucene.Net.Analysis.Hunspell.Tests
+{
+    public class HunspellDictionaryTests
+    {
+        public void WhenParsingAffFileWithAliases_ThenItWorks()
+        {
+            var dico = new HunspellDictionary(
+                ResourceToFile.RscToStream("Lucene.Net.Analysis.Hunspell.Tests.Content.fr-moderne.aff"),
+                ResourceToFile.RscToStream("Lucene.Net.Analysis.Hunspell.Tests.Content.fr-moderne.dic")
+                );
+
+            Assert.True(true);
+        }
+    }
+}
